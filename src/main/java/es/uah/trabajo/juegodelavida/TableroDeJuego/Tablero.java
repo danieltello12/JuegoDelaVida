935fe490ca27133737ad10b3eb15005d5b5640ae
luciabm18 <157727848+luciabm18@users.noreply.github.com>
2024-05-25 18:11:11 +0200

    commit

--- src/main/java/es/uah/trabajo/juegodelavida/TableroDeJuego/Tablero.java
+++ src/main/java/es/uah/trabajo/juegodelavida/TableroDeJuego/Tablero.java
@@ -1138,7 +1138,7 @@ public String dameCadenaFlujo(Partida partida){
             return false;
         }
     }
-    //Hhbnvjhlv
+    //
 
     public Box añadirelementos ( int filas, int columnas, ListaRecursos recursos, ListaELementos individuos) throws
             FileNotFoundException {
